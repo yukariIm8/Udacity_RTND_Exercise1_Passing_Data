ba933ab302c1da5540139828e2d72197ad1c794c
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-09-11 22:51:14 -0700

    style: Adjust indentation.

--- src/App.js
+++ src/App.js
@@ -106,17 +106,17 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
-		<ul>
-		{profiles.map(profile => {
-        	const userName = users[profile.userID].name;
-			const favoriteMovie = movies[profile.favoriteMovieID].name;
-			return (
-            <li key={profile.id}>
-				<p>{`${userName}'s favorite movie is "${favoriteMovie}."`}</p>
-			</li>
+        <ul>
+          {profiles.map(profile => {
+            const userName = users[profile.userID].name;
+            const favoriteMovie = movies[profile.favoriteMovieID].name;
+            return (
+              <li key={profile.id}>
+                <p>{`${userName}'s favorite movie is "${favoriteMovie}."`}</p>
+              </li>
             );
-        })}
-		</ul>
+          })}
+        </ul>
       </div>
     );
   }
